5caf40c4ed5a246cac5d73dce57f74f4a4c7f564
Alina <alinazubanovastudy@gmail.com>
2022-03-18 13:07:14 +0700

    не то, чтобы много изменилось, но я навела порядок в алгоритме лабиринта

--- a-star/a-star.js
+++ a-star/a-star.js
@@ -1,32 +1,48 @@
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var contex = canvas.getContext('2d');
 
-const CellSize=10;
-const Padding=5;
-const wallColor='black';
-const freeColor='white';
+const CellSize = 40;
+const Padding = 5;
+const wallColor = "black";
+const freeColor = "white";
 
-const matrix=createMatrix(10,10);
-const eraser ={x:0,y:0};
-
-drawMaze
-
-function crateMatrix(columns,rows)
-{
-  const matrix=[];
-  for (let y=0; y < rows; y++)
-  {
-    const rows=[];
-    for (let x=0; x<columns;x++)
-    {
+function CreateMatrix(columns, rows) {
+  const matrix = [];
+  for (let y = 0; y < rows; y++) {
+    const rows = [];
+    for (let x = 0; x < columns; x++) {
       rows.push(false);
     }
     matrix.push(rows);
   }
+  return matrix;
 }
 
-function DrawMaze()
-{
-  canvas.width=Padding*2+columns*CellSize;
-  canvas.height=Padding*2+rows*CellSize;
+
+function DrawMaze(columns, rows) {
+  canvas.width = Padding * 2 + columns * CellSize;
+  canvas.height = Padding * 2 + rows * CellSize;
+  contex.beginPath();
+  contex.rect(0, 0, canvas.width, canvas.height);
+  contex.fill();
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < columns; x++) {
+      const color = matrix[y][x] ? freeColor: wallColor;
+      
+      canvas.width = Padding * 2 + columns * CellSize;
+      canvas.height = Padding * 2 + rows * CellSize;
+      contex.beginPath();
+      contex.rect(Padding * 2 + columns * CellSize,
+        Padding * 2 + rows * CellSize,
+        canvas.width,
+        canvas.height);
+      contex.fillStyle = color;
+      contex.fill();
+    }
+  }
 }
+const matrix = CreateMatrix(20, 18);
+const eraser = { x: 0, y: 0 };
+
+DrawMaze(20, 18);
