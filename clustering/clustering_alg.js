c7e54799f56919572f6b4c47170f52bc723dcf11
coto1488 <shorop5@mail.ru>
2022-03-21 12:41:00 +0700

    update cluster

--- clustering/clustering_alg.js
+++ clustering/clustering_alg.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById("canvas"),
          ctx = canvas.getContext("2d");
          var mouse = { x:0, y:0},
-        dots = [],centroidXY = [],minS=[],dotsToCentroid=[],centroidXYBegin=[];
+        dots = [],centroidXY = [],minS=[],dotsToCentroid=[],centroidXYBegin=[],step=0
+        ,color = [],sumcentroidsX =[],sumcentroidsY =[],kolvoCenters=[];
         canvas.clear = function() {
             ctx.clearRect(0, 0, 800, 730);
             dots = [];
@@ -9,6 +10,8 @@ var canvas = document.getElementById("canvas"),
             centroid =[];
             minS=[];
             ctx.fillStyle = "black";
+
+            
         }
         canvas.addEventListener('mousedown',function(e){
             mouse.x = e.pageX - this.offsetLeft;
@@ -19,16 +22,43 @@ var canvas = document.getElementById("canvas"),
             ctx.fill();
         });
         function ClusteringKmeans(){
+
+            function getRandomFloat(min, max) {
+                return Math.random() * (max - min) + min;
+              }
+
+            function getRandomColor() {
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                  color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+              }
+
             var centroid = prompt('Сколь центроидов?',2)
-            for (let i=0;i<centroid;i++){
-                centroidXY[i]=dots[i];
+            step = Math.floor(dots.length/centroid);
+            for (let i=0,j=0;i<dots.length;i+=step){
+                centroidXY[j]=dots[i];
+                j++;
             }
+        // for (let i=0;i<centroid;i++){
+        //     centroidXY[i][0]=getRandomFloat(1, 700);
+        //     centroidXY[i][1]=getRandomFloat(1, 700);
+        // }
+    
+            console.log( centroidXY);
+       
+            for(let i = 0;i<centroid;i++){
+                color[i]=getRandomColor();
+            }
+
            minS = [],dotsToCentroid=[];
            while (centroidXYBegin != centroidXY){
             centroidXYBegin = centroidXY;
             for (let i=0;i<dots.length;i++){ // проходим по всем точкам
                 var s=0, min=10000;
-               // alert(s);
+
                 for (let j=0;j<centroid;j++){ // ищем расстояние между центрами и точками
                         s=Math.sqrt(((centroidXY[j][0]-dots[i][0])**2) + ((centroidXY[j][1]-dots[i][1])**2));
                        
@@ -42,34 +72,23 @@ var canvas = document.getElementById("canvas"),
                    
             }
             for (let h=0;h<dotsToCentroid.length;h++){
-                var sumx0=0,sumy0=0 ,c0=0;
-                var sumx1=0,sumy1=0,c1=0;
-                if (dotsToCentroid[h]==0){
                      ctx.beginPath();
-                     ctx.fillStyle = "orange";
+                     ctx.fillStyle = color[dotsToCentroid[h]];
                      ctx.arc(dots[h][0] , dots[h][1] ,15,0,Math.PI*2);
                      ctx.fill();
-                     
-                     sumx0 += dots[h][0];sumy0 += dots[h][1];c0++;
-                }
-                if (dotsToCentroid[h]==1){
-                    ctx.beginPath();
-                    ctx.fillStyle = "red";
-                    ctx.arc(dots[h][0] , dots[h][1] ,15,0,Math.PI*2);
-                    ctx.fill();
-                    
-                     sumx1 += dots[h][0];sumy1 += dots[h][1];
-                     c1++;
-               }
-            //     console.log(sumx0,c0);
-            //    centroidXY[0][0] = sumx0/c0;
-            // centroidXY[0][1] = sumy0/c0;
-            // centroidXY[1][0] = sumx1/c1;
-            // centroidXY[1][1] = sumy1/c1;
-            // }
+                sumcentroidsX[dotsToCentroid[h]]+=dots[h][0];
+                sumcentroidsY[dotsToCentroid[h]]+=dots[h][1];
+                kolvoCenters[dotsToCentroid[h]]++;
             }
-            // console.log(centroidXY);
-           // var newcentroid1,newcentroid2;
+            for (let h=0;h<centroid.length;h++){
+                centroidXY[h][0]=sumcentroidsX[h]/kolvoCenters[h];
+                centroidXY[h][1]=sumcentroidsY[h]/kolvoCenters[h];
+                // console.log( centroidXY[h][0]);
+                // console.log( centroidXY[h][1]);
+                // console.log(h);
+
+            }
+            
             
             
                 
