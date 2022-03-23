804abad919e130a3cc0e11a53063dda256bab737
Ulatel <ulatlene@mail.ru>
2022-03-23 16:51:13 +0700

    наметки алгоритма
    Здесь пока муравьи не бегают, я не нашла как передавать данные между скриптами, пока залью так, потом буду в ветке делать абру кадабру. Как узнаю буду и дальше разбивать на скрипты, а пока пусть будет это, чтоб я норм вариант не потреяла

--- ants/ants_algs.js
+++ ants/ants_algs.js
@@ -1,24 +1,70 @@
-<png name = 'ant_png' src = 'img\ant.png' />
-let world = {};
-let position ={
-    x: 0,
-    y: 0,
-}
-let colony = {
-    x: 0,
-    y: 0
-};
-class ant {
-    x = colony.x;
-    y = colony.y;
-    eat = 0;
-    speed = 10;
-    direction = 0;
-    direction_update(){
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var ant_count = 100;
+var ant=[];
+var speed = 10;
 
-    }
+ctx.fillRect(10,10,10,10);
+
+
+//-1-стена
+//-2-дом
+
+
+/*var ant_ = new Object;
+var ant_ = {
+    x,
+    y,
+    eat: false,
+    direction: 0,
+    direction_update(){
+        x+= Math.random()%10;
+        y+= Math.random()%10;
+    },
     show(ant_png){
         document.getElementById('#first_img_place').appendChild(img);
     }
-};
+};*/
+
+function createAntColony(){
+    for(var i; i<ant_count;i++){
+        ant[i] = {
+            x: Math.random()%80,
+            y: Math.random()%80,
+            eat: false,
+            direction: 0,
+        }        
+    }
+}
+
+
+function drawAnt(x, y){
+    ctx.fillStyle = 'green';
+    ctx.fillRect(x, y,10,10);
+}
+drawAnt(0,0);
+
+
+
+function ant_algoritm(){
+    drawField();
+    for(var i=0;i<ant_count;i++){
+        ant[i].x+= Math.random()%10;
+        ant[i].y+= Math.random()%10;
+        drawAnt(ant.x, ant.y);
+    }
+}
+
+
+function start_(){
+    createAntColony();
+    console.log(ant);
+    ant_algoritm();
+}
+
+
+start_();
+while(true){
+    ant_algoritm();
+}
 ant.show;
--- ants/paiting.js
+++ ants/paiting.js
@@ -1,63 +1,120 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var world=[];
-var count=0;
+var size_world=80;
+var $world=[];
+var feromon = [];
+var eat_count=0;
 var timer;
+var home = {
+    x: 0,
+    y: 0
+};
 
-canvas.onclick = function(event){
+home.x=10;
+home.y=20;
+
+
+
+canvas.onclick = function(event){//это чтобы рисовать стены
 	var x = event.offsetX;
 	var y = event.offsetY;
 	console.log(x);
 	console.log(y);
 	x = Math.floor(x/10); //300 /10 = 30
 	y = Math.floor(y/10); //300 /10 = 
-	world[y][x]=1;
+	world[y][x]=-1;
+	world[home.x][home.y]=-2;
 	console.log(world);
 	drawField();
 }
 
-function createWorld(){
+
+
+
+
+function build_home(){//это чтобы ставить дом на карту не работает
+	
+	addEventListener("click", function() {
+		
+		var x = offsetY;
+		var y = offsetY;
+		console.log(x);
+		console.log(y);
+		x = Math.floor(x/10); //300 /10 = 30
+		y = Math.floor(y/10); //300 /10 = 
+
+		home.x=x;
+		home.y=y;
+
+		world[y][x]=-2;
+		console.log(world);
+		drawField();
+	});
+}
+
+function drop_food(){//это чтобы ставить дом на карту не работает
+	
+	addEventListener("click", function() {
+		
+		var x = offsetY;
+		var y = offsetY;
+		console.log(x);
+		console.log(y);
+		x = Math.floor(x/10); //300 /10 = 30
+		y = Math.floor(y/10); //300 /10 = 
+
+		home.x=x;
+		home.y=y;
+
+		world[y][x]=1;
+		console.log(world);
+		drawField();
+	});
+}
+
+
+
+function createWorld(){//инициализирует мир
 	var n=size_world, m=size_world;
 	for (var i=0; i<m; i++){
 		world[i]=[];
+		feromon[i]=[];
 		for (var j=0; j<n; j++){
 			world[i][j]=0;
+			feromon[i][j]=0;
 		}
 	}
 }
-size_world=80;
-createWorld();
 
 function drawField(){
 	ctx.clearRect(0, 0, 800, 800);
 	for (var i=0; i<size_world; i++){
 		for (var j=0; j<size_world; j++){
-			if (world[i][j]==1){
+			if (world[i][j]==-1){
+				ctx.fillStyle = 'black';
 				ctx.fillRect(j*10, i*10, 10, 10);
 			}
+			else 
+				if(world[i][j]==-2){
+					ctx.fillStyle = 'red';
+					ctx.fillRect(j*10, i*10, 10, 10);
+				}
+				else
+				if(world[i][j]>0){
+					ctx.fillStyle = 'green';
+					ctx.fillRect(j*10, i*10, 10, 10);
+				}
 		}
 	}
 }
 
-function fpm(i){
-	if(i==0) return size_world;
-	else return i;
-}
-function fpp(i){
-	if(i==size_world-1) return -1;
-	else return i;
-}
 
-function readWorld(){/* вот эта херота для чтения готовой карты https://www.youtube.com/watch?v=p1jqBiqnnxI*/
-    
-    document.querySelector('#file').addEventListener('click', function(){
-        let file =document.getElementById('#file').files[0];
-        let reader = new FileReader();
-        reader.readAsArray(file);
-        reader.onload = function(){
-            world = reader;
-        }
-    })
-}
+createWorld();
 
-document.getElementById('draw').onclick = startLife;
+
+
+eat_count = 2;
+world[13][20]=1;
+world[33][20]=1;
+
+document.getElementById('home').onclick = build_home;
